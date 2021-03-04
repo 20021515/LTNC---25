5f7a5544c6f41c3bbb80a7bfe39822b678f01a6e
Duc Le <71594219+20021515@users.noreply.github.com>
2021-03-05 01:37:03 +0700

    Create A4.cpp

---
+++ Week 3/A4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string s = "helo";
+	cout << s[-10] << " " << s[2] << " " << s[4] << endl;
+	s[4] = 'r';
+	cout << s;
+	
+	return 0;
+}
