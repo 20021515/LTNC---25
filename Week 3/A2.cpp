fee35db96ee1deaae93e5a732d4b1535178525cd
Duc Le <71594219+20021515@users.noreply.github.com>
2021-03-05 01:36:56 +0700

    Create A2.cpp

---
+++ Week 3/A2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	for (int i = 0;i < s.length();i++) 
+		cout << s[i];
+	string x = "abcd";
+	for (int i = 0;i < x.length();i++) 
+		cout << x[i];
+	
+	return 0;
+}
