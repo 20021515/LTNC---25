7c68a764593333c54bfa8ce857f8b49ab95c6b5d
Duc Le <71594219+20021515@users.noreply.github.com>
2021-04-01 00:45:42 +0700

    Update A1.cpp

--- Week 5/A1.cpp
+++ Week 5/A1.cpp
@@ -22,7 +22,10 @@ int main()
     for (int i = 0; i < 3; i++) 
 		cout << (void*)&b[i] << " ";
     cout << endl;
+    
     return 0;
     
-    // K?t qu? gi?ng nhau và ch? khác ? 2 kí t? cu?i
+    //Trong 2 mang co khac nhau ve 3 ki tu cuoi
+    //Trong 1 mang khac nhau 2 ki tu cuoi
+    /*Ket qua khong doi*/
 }
