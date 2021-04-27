906d2e30481c0c98ec834d46a519c7554a175453
Duc Le <71594219+20021515@users.noreply.github.com>
2021-04-27 17:53:06 +0700

    Create A1.cpp

---
+++ Week 9/A1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+char *concat(const char *s1, const char *s2)
+{
+	char *s3 = new char [strlen(s1) + strlen(s2)];
+	
+	s3 = strcpy(s3, s1);
+	s3 = strcat(s3, s2);
+	
+	return s3;
+}
+
+int main()
+{
+	char s1[] = "Hello";
+	char s2[] = "World";
+	
+	cout << concat(s1, s2);
+	
+	return 0;
+}
